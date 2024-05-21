795516b2969c67735cb2c6a46db7450c4f3a5a85
KhushPatibandha <khush.patibandha@gmail.com>
2024-05-21 16:28:54 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -4,25 +4,36 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
-	// Uncomment this block to pass the first stage
-	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close();
+
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer connection.Close();
+
+	buf := make([]byte, 1024);
+	_, err = connection.Read(buf);
+	if err != nil {
+		fmt.Println("Error reading:", err.Error());
+	}
+
+	fmt.Printf("Received data: %v", string(buf));
+	handlePing(connection);
+}
+
+func handlePing(connection net.Conn) {
+	_, err := connection.Write([]byte("+PONG\r\n"));
+	if err != nil {
+		fmt.Println("Error writing:", err.Error());
 	}
 }
