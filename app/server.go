4c37a7870e66d777ba3228a2645ed41b86988012
KhushPatibandha <khush.patibandha@gmail.com>
2024-05-22 15:52:32 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,37 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
 	"net"
 	"os"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
-	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
-	}
-	defer l.Close();
+    port := flag.String("port", "6379", "port to listen on")
+    flag.Parse()
 
-	for {
-		connection, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
-		go handleConnection(connection);
-	}
+    l, err := net.Listen("tcp", "0.0.0.0:"+*port)
+    if err != nil {
+        fmt.Println("Failed to bind to port", *port)
+        os.Exit(1)
+    }
+    defer l.Close()
+
+    for {
+        connection, err := l.Accept()
+        if err != nil {
+            fmt.Println("Error accepting connection: ", err.Error())
+            os.Exit(1)
+        }
+        go handleConnection(connection)
+    }
 }
 
 func handleConnection(connection net.Conn) {
-	defer connection.Close();
-	for  {
-		buf := make([]byte, 1024);
-		_, err := connection.Read(buf);
-		if err != nil {
-			fmt.Println("Error reading:", err.Error());
-		}
-		ParseData(buf, connection);
-	}
+    defer connection.Close()
+    for {
+        buf := make([]byte, 1024)
+        _, err := connection.Read(buf)
+        if err != nil {
+            if err == io.EOF {
+                return
+            }
+            fmt.Println("Error reading:", err.Error())
+        }
+        ParseData(buf, connection)
+    }
 }
+
+// func handleConnection(connection net.Conn) {
+//     defer connection.Close()
+//     for {
+//         buf := make([]byte, 1024)
+//         _, err := connection.Read(buf)
+//         if err != nil {
+//             fmt.Println("Error reading:", err.Error())
+//         }
+//         ParseData(buf, connection)
+//     }
+// }
