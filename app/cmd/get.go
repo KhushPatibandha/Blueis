bea4bfb9f372a6a94cfdbe91211480c0e305e295
KhushPatibandha <khush.patibandha@gmail.com>
2024-06-18 16:26:28 +0530

    minor fix

--- app/cmd/get.go
+++ app/cmd/get.go
@@ -36,41 +36,49 @@ func HandleGet(connection net.Conn, server *typestructs.Server, parts []string, 
 	} else {
 		filePath := dir + "/" + dbfilename;
 
-		file, err := os.Open(filePath)
-		if err != nil {
-			fmt.Println("Error opening RDB file:", err)
-			return
-		}
-		defer file.Close()
-
-		file.Seek(9, 0)
-
-		keyValueMap, err := rdbutil.ReadAllKeyValues(file);
-		if err != nil {
-			fmt.Println("Error reading key-value pairs from RDB file:", err)
-			return
-		}
-
-		// set the values in the map, so that we dont have to read the file again for keys in the rdb file
-		for key, value := range keyValueMap {
-			if(value.ExpiryTime == nil || time.Now().Before(*value.ExpiryTime)) {
-				setGetMap[key] = value.Value;
+		if filePath != "/" {
+			
+			file, err := os.Open(filePath)
+			if err != nil {
+				fmt.Println("Error opening RDB file:", err)
+				return
 			}
-		}
+			defer file.Close()
 
-		value, ok := keyValueMap[parts[4]]
-		if ok {
-			if value.ExpiryTime != nil && time.Now().After(*value.ExpiryTime) {
+			file.Seek(9, 0)
+
+			keyValueMap, err := rdbutil.ReadAllKeyValues(file);
+			if err != nil {
+				fmt.Println("Error reading key-value pairs from RDB file:", err)
+				return
+			}
+
+			// set the values in the map, so that we dont have to read the file again for keys in the rdb file
+			for key, value := range keyValueMap {
+				if(value.ExpiryTime == nil || time.Now().Before(*value.ExpiryTime)) {
+					setGetMap[key] = value.Value;
+				}
+			}
+
+			value, ok := keyValueMap[parts[4]]
+			if ok {
+				if value.ExpiryTime != nil && time.Now().After(*value.ExpiryTime) {
+					_, err := connection.Write([]byte("$-1\r\n"))
+					if err != nil {
+						fmt.Println("Error writing:", err.Error())
+					}
+					return
+				}
+				dataToSend := "$" + strconv.Itoa(len(value.Value)) + "\r\n" + value.Value + "\r\n"
+				_, err := connection.Write([]byte(dataToSend))
+				if err != nil {
+					fmt.Println("Error writing:", err.Error())
+				}
+			} else {
 				_, err := connection.Write([]byte("$-1\r\n"))
 				if err != nil {
 					fmt.Println("Error writing:", err.Error())
 				}
-				return
-			}
-			dataToSend := "$" + strconv.Itoa(len(value.Value)) + "\r\n" + value.Value + "\r\n"
-			_, err := connection.Write([]byte(dataToSend))
-			if err != nil {
-				fmt.Println("Error writing:", err.Error())
 			}
 		} else {
 			_, err := connection.Write([]byte("$-1\r\n"))
