603af5a088334f5fd3f26437bc44d8b881e4e58e
KhushPatibandha <khush.patibandha@gmail.com>
2024-07-02 17:32:53 +0530

    hset cmd added

---
+++ app/cmd/hset.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+
+	typestructs "github.com/codecrafters-io/redis-starter-go/app/typeStructs"
+)
+
+func HandleHset(connection net.Conn, server *typestructs.Server, parts []string, hashMap map[string][]map[string]string, connAndCommands map[net.Conn][]string, dataStr string, flag bool) string {
+	if flag {
+		_, ok := connAndCommands[connection];
+		if ok {
+			connAndCommands[connection] = append(connAndCommands[connection], dataStr);
+			
+			_, err := connection.Write([]byte("+QUEUED\r\n"));
+			if err != nil {
+				fmt.Println("Error writing:", err.Error());
+			}
+			return "+QUEUED\r\n";
+		}
+	}
+
+	server.Offset += len(dataStr);
+	partsLen := len(parts);
+
+	if partsLen < 8 {
+		if flag {
+			_, err := connection.Write([]byte("-ERR wrong number of arguments for command\r\n"));
+			if err != nil {
+				fmt.Println("Error writing:", err.Error());
+			}
+		}
+		return "-ERR wrong number of arguments for command\r\n";
+	}
+
+	hashKeyName := parts[4];
+	valueMapList, ok := hashMap[hashKeyName];
+	if !ok {
+		valueMapList = make([]map[string]string, 0);
+	}
+
+	count := 0;
+
+	for i := 5; i < partsLen; i+=4 {
+		field := parts[i + 1];
+		value := parts[i + 3];
+		valueMap := make(map[string]string);
+		valueMap[field] = value;
+		valueMapList = append(valueMapList, valueMap);
+		count++;
+	}
+
+	hashMap[hashKeyName] = valueMapList;
+
+	dataToSend := ":" + strconv.Itoa(count) + "\r\n";
+	if flag {
+		_, err := connection.Write([]byte(dataToSend));
+		if err != nil {
+			fmt.Println("Error writing:", err.Error());
+		}
+	}
+	return dataToSend;
+}
